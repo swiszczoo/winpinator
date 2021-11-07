ade1eba4e8e1ea0598f18d754bf9c91945707753
Łukasz Świszcz <luk.swiszcz@gmail.com>
2021-11-08 00:16:51 +0100

    Fix translation script so that it doesn't append to previous .pot file

--- build/post_compile.bat
+++ build/post_compile.bat
@@ -11,7 +11,7 @@ REM -------- xgettext --------
 echo Run xgettext...
 cd "%3"
 powershell "Get-ChildItem -Recurse *.?pp | Resolve-Path -Relative" > "../po/filelist.txt"
-xgettext -C -k_ -kwxTRANSLATE -kwxPLURAL:1,2 -f "%3/../po/filelist.txt" -j --add-comments=TRANSLATORS --no-wrap -o "%3/../po/winpinator.pot"
+xgettext -C -k_ -kwxTRANSLATE -kwxPLURAL:1,2 -f "%3/../po/filelist.txt" --add-comments=TRANSLATORS --no-wrap -o "%3/../po/winpinator.pot"
 del "../po/filelist.txt"
 
 REM -------- compile translations --------
